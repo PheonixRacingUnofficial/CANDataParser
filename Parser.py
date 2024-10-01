eb08812f512d874c0a9dea26ee2166d2bc24f129
Chase <cwasalaski7917@floridapoly.edu>
2024-10-01 01:31:20 -0400

    Added TRC file parsing and proper error handling (not really proper but yknow)

--- Parser.py
+++ Parser.py
@@ -1,4 +1,5 @@
 import datetime
+import re
 import struct
 
 def hex_to_int8(hex_str):
@@ -61,167 +62,190 @@ def parse_cmu_sensor(sensor_id: int, sensor_data: str, time: str) -> str:
     return out
 
 def parse_can_line(data: str, show_data: bool) -> str:
-    # Bad Error Handling :)
-    if data == "no data":
-        return None
-    # data: str = input("Enter CAN data line: ")
-    timestamp = data[1:18]
-    data: str = trim_can_input(data)
-    sensor_id: str = get_sensor_id(data)
-    sensor_id_int: int = int(sensor_id, 16)
-    sensor_data: str = get_sensor_data(data)
-    if show_data:
-        print(f"Timestamp: {timestamp}")
-        print(f"Sensor ID: {sensor_id}")
-        print(f"Sensor ID (int): {sensor_id_int}")
-        print(f"Sensor Data: {sensor_data}")
+    # Better error handling
+    if data[0] == '(':
+        # Normal CAN data, continue as normal
+        # print("This is a CAN log line")
+        ...
+    elif data[0] == ';':
+        # TRC Header Data, ignore for now, can be used for finding timestamps, but I don't wanna
+        # print("This is a trc data header line")
+        ...
+    elif bool(re.search(r'\d+\)', data)):
+        # TRC Log Data, translate to CAN data before processing
+        # print("This is a trc log line")
 
-    # convert timestamp from unix time to human readable time
-    out = f'[{str(datetime.datetime.fromtimestamp(float(timestamp)))}] '
+        can = "(0000000000.000000) can0 "
+        data = data[33:]
+        can += data[:3] + '#' + data[6:].replace(' ', '')
+        data = can
 
-    # CMU Sensor are parsed differently here due to the range of values that they can have
-    # that all have the same exact code
-    if 0x301 <= sensor_id_int <= 0x3F3:
-        # print("This should be a CMU sensor")
-        return parse_cmu_sensor(sensor_id_int, sensor_data, out)
+        # print(can)
+        ...
+    else:
+        return f"Unsupported data format; line: {data}"
 
-    match sensor_id_int:
-        # Heartbeat Sensor
-        case 0x300:
-            out += "BMU Heartbeat Sensor; "
-            hb_id = hex_to_uint32(sensor_data[:8])
-            hb_serial_number = hex_to_uint32(sensor_data[8:16])
-            out += f"Device ID: {hb_id}; Serial Number: {hb_serial_number}"
+    try:
+        timestamp = data[1:18]
+        data: str = trim_can_input(data)
+        sensor_id: str = get_sensor_id(data)
+        sensor_id_int: int = int(sensor_id, 16)
+        sensor_data: str = get_sensor_data(data)
+        if show_data:
+            print(f"Timestamp: {timestamp}")
+            print(f"Sensor ID: {sensor_id}")
+            print(f"Sensor ID (int): {sensor_id_int}")
+            print(f"Sensor Data: {sensor_data}")
 
-            return out
+        # convert timestamp from unix time to human-readable time
+        out = f'[{str(datetime.datetime.fromtimestamp(float(timestamp)))}] '
 
-        case 0x3F4:
-            out += "Pack SoC; "
-            pack_soc = hex_to_float(sensor_data[:8])
-            pack_soc_percent = hex_to_float(sensor_data[8:16])
-            out += f"Pack SoC: {pack_soc}aH; Pack SoC Percent: {pack_soc_percent}"
+        # CMU Sensor are parsed differently here due to the range of values that they can have
+        # that all have the same exact code
+        if 0x301 <= sensor_id_int <= 0x3F3:
+            # print("This should be a CMU sensor")
+            return parse_cmu_sensor(sensor_id_int, sensor_data, out)
 
-            return out
+        match sensor_id_int:
+            # Heartbeat Sensor
+            case 0x300:
+                out += "BMU Heartbeat Sensor; "
+                hb_id = hex_to_uint32(sensor_data[:8])
+                hb_serial_number = hex_to_uint32(sensor_data[8:16])
+                out += f"Device ID: {hb_id}; Serial Number: {hb_serial_number}"
 
-        case 0x3F5:
-            out += "Pack Balance SoC; "
-            pack_balance_soc = hex_to_float(sensor_data[:8])
-            pack_balance_soc_percent = hex_to_float(sensor_data[8:16])
-            out += f"Pack Balance SoC: {pack_balance_soc}aH; Pack Balance SoC Percent: {pack_balance_soc_percent}"
+                return out
 
-            return out
+            case 0x3F4:
+                out += "Pack SoC; "
+                pack_soc = hex_to_float(sensor_data[:8])
+                pack_soc_percent = hex_to_float(sensor_data[8:16])
+                out += f"Pack SoC: {pack_soc}aH; Pack SoC Percent: {pack_soc_percent}"
 
-        case 0x3F6:
-            # Note, data comes through this channel 10x as fast as other channels
-            # Values are calculated based on preconfigured values, may result in errors with sample data?
-            out += "Charger Control Info; "
-            charging_cell_voltage_error = hex_to_int16(sensor_data[:4])
-            charging_cell_temp_margin = hex_to_int16(sensor_data[4:8]) / 10 # should error on a zero value
-            discharge_cell_voltage_error = hex_to_int16(sensor_data[8:12])
-            total_pack_capacity = hex_to_uint16(sensor_data[12:16]) # preset value?
-            out += f"Charging Cell Voltage Error: {charging_cell_voltage_error}mV; Charging Cell Temp Margin: {charging_cell_temp_margin}°C; Discharge Cell Voltage Error: {discharge_cell_voltage_error}mV; Total Pack Capacity: {total_pack_capacity}Ah"
+                return out
 
-            return out
+            case 0x3F5:
+                out += "Pack Balance SoC; "
+                pack_balance_soc = hex_to_float(sensor_data[:8])
+                pack_balance_soc_percent = hex_to_float(sensor_data[8:16])
+                out += f"Pack Balance SoC: {pack_balance_soc}aH; Pack Balance SoC Percent: {pack_balance_soc_percent}"
 
-        case 0x3F7:
-            # print(sensor_data)
-            out += "Precharge Status; "
-            precharge_contactor_status = hex_to_uint8(sensor_data[:2])
-            precharge_state = hex_to_uint8(sensor_data[2:4])
-            match precharge_state:
-                case 0:
-                    precharge_state = f"Error ({precharge_state})"
-                case 1:
-                    precharge_state = f"Idle ({precharge_state})"
-                case 2:
-                    precharge_state = f"Measure ({precharge_state})"
-                case 3:
-                    precharge_state = f"Precharge ({precharge_state})"
-                case 4:
-                    precharge_state = f"Run ({precharge_state})"
-                case 5:
-                    precharge_state = f"Enable Pack ({precharge_state})"
+                return out
 
-            contactor_supply_voltage = hex_to_uint16(sensor_data[4:8]) # could be useless?
-            # 8 : 12 garbage
-            precharge_timer_status = hex_to_uint8(sensor_data[12:14]) # ignore if timeout is disabled
-            match precharge_timer_status:
-                case 0:
-                    precharge_timer_status = f"Disabled ({precharge_timer_status})"
-                case 1:
-                    precharge_timer_status = f"Enabled ({precharge_timer_status})"
-            precharge_timer_value = hex_to_uint8(sensor_data[14:16]) * 10
-            out += f"Precharge Contactor Status: {precharge_contactor_status}; Precharge State: {precharge_state}; Contactor Supply Voltage: {contactor_supply_voltage}mV; Precharge Timer Status: {precharge_timer_status}; Precharge Timer Value: {precharge_timer_value}ms"
+            case 0x3F6:
+                # Note, data comes through this channel 10x as fast as other channels
+                # Values are calculated based on preconfigured values, may result in errors with sample data?
+                out += "Charger Control Info; "
+                charging_cell_voltage_error = hex_to_int16(sensor_data[:4])
+                charging_cell_temp_margin = hex_to_int16(sensor_data[4:8]) / 10 # should error on a zero value
+                discharge_cell_voltage_error = hex_to_int16(sensor_data[8:12])
+                total_pack_capacity = hex_to_uint16(sensor_data[12:16]) # preset value?
+                out += f"Charging Cell Voltage Error: {charging_cell_voltage_error}mV; Charging Cell Temp Margin: {charging_cell_temp_margin}°C; Discharge Cell Voltage Error: {discharge_cell_voltage_error}mV; Total Pack Capacity: {total_pack_capacity}Ah"
 
-            return out
+                return out
 
-        case 0x3F8:
-            # Note that this is a 10x faster channel
-            out += "Min / Max Cell Voltage; "
-            min_cell_voltage = hex_to_uint16(sensor_data[:4])
-            max_cell_voltage = hex_to_uint16(sensor_data[4:8])
-            cmu_with_min_voltage = hex_to_uint8(sensor_data[8:10])
-            cell_with_min_voltage = hex_to_uint8(sensor_data[10:12])
-            cmu_with_max_voltage = hex_to_uint8(sensor_data[12:14])
-            cell_with_max_voltage = hex_to_uint8(sensor_data[14:16])
-            out += f"Min Cell Voltage: {min_cell_voltage}mV; Max Cell Voltage: {max_cell_voltage}mV; CMU with Min Voltage: {cmu_with_min_voltage}; Cell with Min Voltage: {cell_with_min_voltage}; CMU with Max Voltage: {cmu_with_max_voltage}; Cell with Max Voltage: {cell_with_max_voltage}"
+            case 0x3F7:
+                # print(sensor_data)
+                out += "Precharge Status; "
+                precharge_contactor_status = hex_to_uint8(sensor_data[:2])
+                precharge_state = hex_to_uint8(sensor_data[2:4])
+                match precharge_state:
+                    case 0:
+                        precharge_state = f"Error ({precharge_state})"
+                    case 1:
+                        precharge_state = f"Idle ({precharge_state})"
+                    case 2:
+                        precharge_state = f"Measure ({precharge_state})"
+                    case 3:
+                        precharge_state = f"Precharge ({precharge_state})"
+                    case 4:
+                        precharge_state = f"Run ({precharge_state})"
+                    case 5:
+                        precharge_state = f"Enable Pack ({precharge_state})"
 
-            return out
+                contactor_supply_voltage = hex_to_uint16(sensor_data[4:8]) # could be useless?
+                # 8 : 12 garbage
+                precharge_timer_status = hex_to_uint8(sensor_data[12:14]) # ignore if timeout is disabled
+                match precharge_timer_status:
+                    case 0:
+                        precharge_timer_status = f"Disabled ({precharge_timer_status})"
+                    case 1:
+                        precharge_timer_status = f"Enabled ({precharge_timer_status})"
+                precharge_timer_value = hex_to_uint8(sensor_data[14:16]) * 10
+                out += f"Precharge Contactor Status: {precharge_contactor_status}; Precharge State: {precharge_state}; Contactor Supply Voltage: {contactor_supply_voltage}mV; Precharge Timer Status: {precharge_timer_status}; Precharge Timer Value: {precharge_timer_value}ms"
 
-        case 0x3F9:
-            out += "Min / Max Cell Temp; "
-            min_cell_temp = hex_to_uint16(sensor_data[:4]) / 10
-            max_cell_temp = hex_to_uint16(sensor_data[4:8]) / 10
-            cmu_with_min_temp = hex_to_uint8(sensor_data[8:10])
-            cell_with_min_temp = hex_to_uint8(sensor_data[10:12]) # unused
-            cmu_with_max_temp = hex_to_uint8(sensor_data[12:14])
-            cell_with_max_temp = hex_to_uint8(sensor_data[14:16]) # unused
-            out += f"Min Cell Temp: {min_cell_temp}°C; Max Cell Temp: {max_cell_temp}°C; CMU with Min Temp: {cmu_with_min_temp}; CMU with Max Temp: {cmu_with_max_temp};"
+                return out
 
-            return out
+            case 0x3F8:
+                # Note that this is a 10x faster channel
+                out += "Min / Max Cell Voltage; "
+                min_cell_voltage = hex_to_uint16(sensor_data[:4])
+                max_cell_voltage = hex_to_uint16(sensor_data[4:8])
+                cmu_with_min_voltage = hex_to_uint8(sensor_data[8:10])
+                cell_with_min_voltage = hex_to_uint8(sensor_data[10:12])
+                cmu_with_max_voltage = hex_to_uint8(sensor_data[12:14])
+                cell_with_max_voltage = hex_to_uint8(sensor_data[14:16])
+                out += f"Min Cell Voltage: {min_cell_voltage}mV; Max Cell Voltage: {max_cell_voltage}mV; CMU with Min Voltage: {cmu_with_min_voltage}; Cell with Min Voltage: {cell_with_min_voltage}; CMU with Max Voltage: {cmu_with_max_voltage}; Cell with Max Voltage: {cell_with_max_voltage}"
 
-        case 0x3FA:
-            out += "Battery Pack Info; "
-            pack_voltage = hex_to_uint32(sensor_data[:8])
-            pack_current = hex_to_int32(sensor_data[8:16])
-            out += f"Pack Voltage: {pack_voltage}mV; Pack Current: {pack_current}mA"
+                return out
 
-            return out
+            case 0x3F9:
+                out += "Min / Max Cell Temp; "
+                min_cell_temp = hex_to_uint16(sensor_data[:4]) / 10
+                max_cell_temp = hex_to_uint16(sensor_data[4:8]) / 10
+                cmu_with_min_temp = hex_to_uint8(sensor_data[8:10])
+                cell_with_min_temp = hex_to_uint8(sensor_data[10:12]) # unused
+                cmu_with_max_temp = hex_to_uint8(sensor_data[12:14])
+                cell_with_max_temp = hex_to_uint8(sensor_data[14:16]) # unused
+                out += f"Min Cell Temp: {min_cell_temp}°C; Max Cell Temp: {max_cell_temp}°C; CMU with Min Temp: {cmu_with_min_temp}; CMU with Max Temp: {cmu_with_max_temp};"
 
-        case 0x3FB:
-            out += "Battery Pack Status; "
-            balance_voltage_threshold_rising = hex_to_uint16(sensor_data[:4])
-            balance_voltage_threshold_falling = hex_to_uint16(sensor_data[4:8])
-            pack_status = hex_to_uint8(sensor_data[8:10]) # unused potentially?
-            cmu_count = hex_to_uint8(sensor_data[10:12])
-            bmu_firmware_build_number = hex_to_uint16(sensor_data[12:16])
-            out += f"Balance Voltage Threshold Rising: {balance_voltage_threshold_rising}mV; Balance Voltage Threshold Falling: {balance_voltage_threshold_falling}mV; CMU Count: {cmu_count}; BMU Firmware Build Number: {bmu_firmware_build_number}"
+                return out
 
-            return out
+            case 0x3FA:
+                out += "Battery Pack Info; "
+                pack_voltage = hex_to_uint32(sensor_data[:8])
+                pack_current = hex_to_int32(sensor_data[8:16])
+                out += f"Pack Voltage: {pack_voltage}mV; Pack Current: {pack_current}mA"
 
-        case 0x3FC:
-            out += "Battery Pack Fan Status; "
-            fan_0_speed = hex_to_uint16(sensor_data[:4])
-            fan_1_speed = hex_to_uint16(sensor_data[4:8])
-            current_consumption_fans_and_contactors = hex_to_uint16(sensor_data[8:12])
-            current_consumption_cmus = hex_to_uint16(sensor_data[12:16])
-            out += f"Fan 0 Speed: {fan_0_speed}rpm; Fan 1 Speed: {fan_1_speed}rpm; Current Consumption Fans and Contactors: {current_consumption_fans_and_contactors}mA; Current Consumption CMUs: {current_consumption_cmus}mA"
+                return out
 
-            return out
+            case 0x3FB:
+                out += "Battery Pack Status; "
+                balance_voltage_threshold_rising = hex_to_uint16(sensor_data[:4])
+                balance_voltage_threshold_falling = hex_to_uint16(sensor_data[4:8])
+                pack_status = hex_to_uint8(sensor_data[8:10]) # unused potentially?
+                cmu_count = hex_to_uint8(sensor_data[10:12])
+                bmu_firmware_build_number = hex_to_uint16(sensor_data[12:16])
+                out += f"Balance Voltage Threshold Rising: {balance_voltage_threshold_rising}mV; Balance Voltage Threshold Falling: {balance_voltage_threshold_falling}mV; CMU Count: {cmu_count}; BMU Firmware Build Number: {bmu_firmware_build_number}"
 
-        case 0x3FD:
-            out += "Extended Battery Pack Info; "
-            pack_status = hex_to_uint32(sensor_data[:8])
-            bmu_hardware_version = hex_to_uint8(sensor_data[8:10])
-            bmu_model_id = hex_to_uint8(sensor_data[10:12])
-            # 12 : 16 garbage
-            out += f"Pack Status: {pack_status}; BMU Hardware Version: {bmu_hardware_version}; BMU Model ID: {bmu_model_id}"
+                return out
 
-            return out
+            case 0x3FC:
+                out += "Battery Pack Fan Status; "
+                fan_0_speed = hex_to_uint16(sensor_data[:4])
+                fan_1_speed = hex_to_uint16(sensor_data[4:8])
+                current_consumption_fans_and_contactors = hex_to_uint16(sensor_data[8:12])
+                current_consumption_cmus = hex_to_uint16(sensor_data[12:16])
+                out += f"Fan 0 Speed: {fan_0_speed}rpm; Fan 1 Speed: {fan_1_speed}rpm; Current Consumption Fans and Contactors: {current_consumption_fans_and_contactors}mA; Current Consumption CMUs: {current_consumption_cmus}mA"
 
+                return out
 
-        case _:
-            return f"Sensor ID {sensor_id} not recognized; Data: {sensor_data[:-1]}"
+            case 0x3FD:
+                out += "Extended Battery Pack Info; "
+                pack_status = hex_to_uint32(sensor_data[:8])
+                bmu_hardware_version = hex_to_uint8(sensor_data[8:10])
+                bmu_model_id = hex_to_uint8(sensor_data[10:12])
+                # 12 : 16 garbage
+                out += f"Pack Status: {pack_status}; BMU Hardware Version: {bmu_hardware_version}; BMU Model ID: {bmu_model_id}"
+
+                return out
+
+            case _:
+                return f"Sensor ID {sensor_id} not recognized; Data: {sensor_data[:-1]}"
+
+    except Exception as e:
+        print("Something happen:(")
+        return f"Error: {e}"
 
 def trim_can_input(data: str) -> str:
     """ Trim the CAN data input to only the data portion """
