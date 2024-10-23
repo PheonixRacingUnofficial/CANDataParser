f236c5842d079e9f069f33ac7b16df24d04429e5
Chase <cwasalaski7917@floridapoly.edu>
2024-10-23 15:40:50 -0400

    Change USB port

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main():
         gui_thread.start()
 
     if args.serial:
-        ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+        ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
         ser.reset_input_buffer()
         while True:
             if ser.in_waiting > 0:
