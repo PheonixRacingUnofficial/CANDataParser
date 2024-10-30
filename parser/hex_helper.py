576cc0b5d6d31dd6b68b36e7ffc6df7cb32db9fd
Chase <cwasalaski7917@floridapoly.edu>
2024-10-30 13:11:28 -0400

    Added some typing

--- parser/hex_helper.py
+++ parser/hex_helper.py
@@ -1,34 +1,34 @@
 import struct
 
-def hex_to_int8(hex_str):
+def hex_to_int8(hex_str) -> int:
     """ Convert a hex string to a signed 8-bit integer """
     return struct.unpack('>b', bytes.fromhex(hex_inverter(hex_str)))[0]
 
-def hex_to_uint8(hex_str):
+def hex_to_uint8(hex_str) -> int:
     """ Convert a hex string to an unsigned 8-bit integer """
     return struct.unpack('>B', bytes.fromhex(hex_inverter(hex_str)))[0]
 
-def hex_to_int16(hex_str):
+def hex_to_int16(hex_str) -> int:
     """ Convert a hex string to a signed 16-bit integer """
     return struct.unpack('>h', bytes.fromhex(hex_inverter(hex_str)))[0]
 
-def hex_to_uint16(hex_str):
+def hex_to_uint16(hex_str) -> int:
     """ Convert a hex string to an unsigned 16-bit integer """
     return struct.unpack('>H', bytes.fromhex(hex_inverter(hex_str)))[0]
 
-def hex_to_int32(hex_str):
+def hex_to_int32(hex_str) -> int:
     """ Convert a hex string to a signed 32-bit integer """
     return struct.unpack('>i', bytes.fromhex(hex_inverter(hex_str)))[0]
 
-def hex_to_uint32(hex_str):
+def hex_to_uint32(hex_str) -> int:
     """ Convert a hex string to an unsigned 32-bit integer """
     return struct.unpack('>I', bytes.fromhex(hex_inverter(hex_str)))[0]
 
-def hex_to_float(hex_str):
+def hex_to_float(hex_str) -> float:
     """ Convert a hex string to a 32-bit float """
     return struct.unpack('>f', bytes.fromhex(hex_inverter(hex_str)))[0]
 
-def hex_to_bits(hex_str):
+def hex_to_bits(hex_str) -> str:
     """ Convert a hex string to its equivalent binary string """
     # convert a string of hex values to its equivalent binary string then fill in the missing 0s
     return bin(int(hex_inverter(hex_str), 16))[2:].zfill(len(hex_str) * 4)
--- parser/logger.py
+++ parser/logger.py
@@ -6,17 +6,20 @@ class Logger:
         self.is_debug = is_debug
         self.is_log = is_log
 
-    def get_debug(self):
+    def get_debug(self) -> bool:
         return self.is_debug
 
-    def info(self, message):
+    def get_log(self) -> bool:
+        return self.is_log
+
+    def info(self, message) -> None:
         if type(message) == list:
             for msg in message:
                 print(f"[INFO] / {msg}")
         else:
             print(f"[INFO / {message}")
 
-    def debug(self, message):
+    def debug(self, message) -> None:
         if self.is_debug:
             if type(message) == list:
                 for msg in message:
@@ -24,28 +27,28 @@ class Logger:
             else:
                 print(f"[DEBUG] {message}")
 
-    def python_error(self, message):
+    def python_error(self, message) -> None:
         if type(message) == list:
             for msg in message:
                 print(f"[PYTHON ERROR] {msg}")
         else:
             print(f"[PYTHON ERROR] {message}")
 
-    def data_error(self, message):
+    def data_error(self, message) -> None:
         if type(message) == list:
             for msg in message:
                 print(f"[DATA ERROR] {msg}")
         else:
             print(f"[DATA ERROR] {message}")
 
-    def value_error(self, message):
+    def value_error(self, message) -> None:
         if type(message) == list:
             for msg in message:
                 print(f"[VALUE ERROR] {msg}")
         else:
             print(f"[VALUE ERROR] {message}")
 
-    def sensor_error(self, message):
+    def sensor_error(self, message) -> None:
         if type(message) == list:
             for msg in message:
                 print(f"[SENSOR ERROR / {msg}")
