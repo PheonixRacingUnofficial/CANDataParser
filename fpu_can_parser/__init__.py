717b6b9a7aaea36bf4ae0f7dc8972b41141e66f5
Chase <cwasalaski7917@floridapoly.edu>
2025-04-06 07:18:41 -0400

    Added gui module

--- fpu_can_parser/__init__.py
+++ fpu_can_parser/__init__.py
@@ -1,4 +1,5 @@
 from fpu_can_parser.parser import hex_helper, logger, sensor_manager, sensor, parser
+from fpu_can_parser.gui import gui
 
-__all__ = ['hex_helper', 'logger', 'sensor_manager', 'sensor', 'parser']
+__all__ = ['hex_helper', 'logger', 'sensor_manager', 'sensor', 'parser', 'gui']
 
